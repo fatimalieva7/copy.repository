694839c0a44df5a6eea85012ce7a18ade6d66db7
Фатима <enemenem45@gmail.com>
2025-04-08 10:08:40 +0600

    The main app is ready, pages index and about

--- main/views.py
+++ main/views.py
@@ -6,13 +6,19 @@ def index(request):
     context = {
         'title': 'Home',
         'content': 'Главная страница магазина - HOME',
-        'list': ['first', 'second'],
-        'dict': {'first': 1},
-        'is_authenticated': False
+
     }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request):
-    return HttpResponse('About page')
+    context = {
+        'title': 'Home',
+        'content': 'Это страница о нас ',
+        'text_on_page': 'Страница о нас магазина Home',
+
+    }
+
+    return render(request, 'main/about.html', context)
+
