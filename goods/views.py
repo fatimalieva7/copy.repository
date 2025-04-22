303b75ff2ef8bf3b9695a4f8ebe7c8c0d78a96e9
Фатима <enemenem45@gmail.com>
2025-04-22 19:30:47 +0600

    url адрес добавила

--- goods/views.py
+++ goods/views.py
@@ -4,14 +4,15 @@ from django.shortcuts import get_list_or_404, render
 from django.core.paginator import Paginator
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+    page = request.GET.get('page', 1)
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
     paginator = Paginator(goods, 3)  # 6 товаров на странице
-    current_page = paginator.page(page)  # Получаем текущую страницу
+    current_page = paginator.page(int(page))  # Получаем текущую страницу
 
 
 
