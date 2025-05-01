e7a3bb9f7ca50c2a2147b41ef9cd9f13be279227
Фатима <enemenem45@gmail.com>
2025-05-01 18:47:26 +0600

    Ошибка происходит, переходы на страницу url фдрес не работает

--- goods/views.py
+++ goods/views.py
@@ -6,16 +6,24 @@ from django.core.paginator import Paginator
 
 def catalog(request, category_slug):
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', 'default')
+
+
     if category_slug == 'all':
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by == order_by != 'default':
+        goods = goods.order_by(order_by)
+    
+
     paginator = Paginator(goods, 3)  # 6 товаров на странице
     current_page = paginator.page(int(page))  # Получаем текущую страницу
-
-
-
 
     context = {
         'title': 'Главная каталог ',
